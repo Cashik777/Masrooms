c4ee03116826bfa6564d56fd4a2fad84b146c8b9
Cashik777 <dmitrijvelikij921@gmail.com>
2025-07-12 04:53:54 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'mushroom-emporium-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/main.css',
+  '/js/man.js',
+  '/images/logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
